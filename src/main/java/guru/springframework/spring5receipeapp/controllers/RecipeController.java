29a7819798e7c21eb17f1ec23173351c37672b3a
Ageddamuri <Ageddamuri@DESKTOP-G32DIOL>
2020-01-14 20:47:51 +0530

    Data validation and displaying with error msg with Thymeleaf

--- src/main/java/guru/springframework/spring5receipeapp/controllers/RecipeController.java
+++ src/main/java/guru/springframework/spring5receipeapp/controllers/RecipeController.java
@@ -1,8 +1,11 @@
 package guru.springframework.spring5receipeapp.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -22,6 +25,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Controller
 public class RecipeController {
+	private static final String RECIPE_RECIPEFORM_URL = "recipe/recipeform";
 
     private final RecipeService recipeService;
 
@@ -46,13 +50,28 @@ public class RecipeController {
     @GetMapping("recipe/{id}/update")
     public String updateRecipe(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
-        return  "recipe/recipeform";
+        return  RECIPE_RECIPEFORM_URL;
     }
-
+ 
+     /*
+      * This bindingResult holds the validated results.
+      * @Valid @ModelAttribute("recipe") --> validates  and we can use the recipe in html while displaying the error msg.
+    	
+      * 
+      */
     @PostMapping("recipe")
-    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
-        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+    public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand command,BindingResult bindingResult){
+    	
+    	if(bindingResult.hasErrors()){
 
+            bindingResult.getAllErrors().forEach(objectError -> {
+                log.debug(objectError.toString());
+            });
+
+            return RECIPE_RECIPEFORM_URL;
+        }
+    	
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
         return "redirect:/recipe/" + savedCommand.getId() + "/show";
     }
 
@@ -78,19 +97,6 @@ public class RecipeController {
         modelAndView.addObject("exception",exception);
 
         return modelAndView;
-    }
-    
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(NumberFormatException.class)
-    public ModelAndView handleNumberFormatException(Exception exception){
-
-        log.error("Handling not found exception");
-
-        ModelAndView modelAndView = new ModelAndView();
-
-        modelAndView.setViewName("400error");
-        modelAndView.addObject("exception",exception);
-
-        return modelAndView;
-    }
+    }  
+   
 }
